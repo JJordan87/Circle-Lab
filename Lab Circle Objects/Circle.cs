13ef6a0fdabed8cc68c19b445bf58b348ecd2908
Jon <JJKing619@gmail.com>
2022-02-14 09:11:46 -0500

    first commit

---
+++ Lab Circle Objects/Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Circle_Objects
+{
+    internal class Circle
+    {
+        public double Radius { get; set; }
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+
+        public double CalculateCircumference()
+        {
+
+            double circumference = 2 * Math.PI * Radius;
+            return circumference;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+             string formattedCircumference = FormatNumber(CalculateCircumference());
+             return formattedCircumference;
+        }
+
+        public double CalculateArea()
+        {
+            double area = Math.PI * Math.Pow(Radius, 2);
+            return area;
+        }
+
+        public string CalculateFormattedArea(double area)
+        {
+            string areaFormatted = FormatNumber(CalculateArea());
+            return areaFormatted;
+        }
+
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x, 2).ToString();
+        }
+    }
+}
---
+++ Lab Circle Objects/Program.cs
@@ -0,0 +1,36 @@
+﻿using Lab_Circle_Objects;
+
+bool runProgram = true;
+
+Console.WriteLine("Welcome to the Circle Tester");
+
+while (runProgram)
+{
+    Console.Write("Enter a radius: ");
+    double userInput = double.Parse(Console.ReadLine());
+    Circle userCircle = new Circle(userInput);
+    Console.WriteLine($"Cicumference: {userCircle.CalculateFormattedCircumference()}");
+    Console.WriteLine($"Area: {userCircle.CalculateFormattedArea}");
+
+    Console.WriteLine("Continue? (y/n)");
+    string userChoice = Console.ReadLine().ToLower().Trim();
+
+    while (true)
+    {
+        if(userChoice == "y")
+        {
+            runProgram = true;
+            break;
+        }
+        else if (userChoice == "n")
+        {
+            Console.WriteLine("Thank you for using the Circle Tester.");
+            runProgram=false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("That is not a valid input.");
+        }
+    }
+}
